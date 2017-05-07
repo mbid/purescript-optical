834ce199a8d0074faa759732061c00dd12e7efe5
Martin Bidlingmaier <martin.bidlingmaier@gmail.com>
2017-05-07 17:31:55 +0200

    Initial commit

---
+++ src/Control/Monad/State/Reified.purs
@@ -0,0 +1,107 @@
+module Control.Monad.State.Reified
+( MonadStateV
+, fromStateFunction
+, fromGetPutFunctions
+, mapMonad
+, mapState
+)
+where
+
+import Prelude
+import Control.Monad.State (class MonadState)
+import Control.Monad.State.Class (get, gets, modify, put, state)
+import Data.Tuple (Tuple(..))
+import Optic.Core (over, set, view)
+import Optic.Types (Lens')
+
+-- | The datum of a `MonadState`, reified as record.  
+-- | `mapMonad` and `mapState` induce the structure of a bifunctor
+-- |`MonadStateV :: Monad x Lens' -> Type`.
+type MonadStateV s m = 
+  { state :: forall a. (s -> Tuple a s) -> m a
+  , get :: m s
+  , gets :: forall a. (s -> a) -> m a
+  , put :: s -> m Unit
+  , modify :: (s -> s) -> m Unit
+  }
+
+reifiedStateClass :: forall m s. MonadState s m => MonadStateV s m
+reifiedStateClass = { state, get, gets, put, modify }
+
+-- | Create a complete `MonadStateV` given just `state`, using default
+-- | implementations for the other functions.
+fromStateFunction ::
+  forall s m. (forall a. (s -> Tuple a s) -> m a) -> MonadStateV s m
+fromStateFunction state = { state, get, gets, put, modify }
+  where
+    gets :: forall a. (s -> a) -> m a
+    gets f = state \s -> Tuple (f s) s
+    get :: m s
+    get = gets id
+    modify :: (s -> s) -> m Unit
+    modify f = state \s -> Tuple unit (f s)
+    put :: s -> m Unit
+    put s = modify $ const s
+
+-- | Create a complete `MonadStateV` given just `get` and `put`, using default
+-- | implementations for the other functions.
+fromGetPutFunctions ::
+  forall s m. Monad m => m s -> (s -> m Unit) -> MonadStateV s m
+fromGetPutFunctions get put = { state, get, gets, put, modify }
+  where
+    state :: forall a. (s -> Tuple a s) -> m a
+    state f = do
+      Tuple x s <- f <$> get
+      put s
+      pure x
+    gets :: forall a. (s -> a) -> m a
+    gets f = f <$> get
+    modify :: (s -> s) -> m Unit
+    modify f = get >>= f >>> put
+
+-- | Change the underlying monad of a `MonadStateV`, given a morphism of monads.
+mapMonad :: forall s m n. (m ~> n) -> MonadStateV s m -> MonadStateV s n
+mapMonad nat { state, get, gets, put, modify } =
+  { state: state'
+  , get: get'
+  , gets: gets'
+  , put: put'
+  , modify: modify'
+  }
+  where
+    state' :: forall a. (s -> Tuple a s) -> n a
+    state' f = nat $ state f
+    get' :: n s
+    get' = nat get
+    gets' :: forall a. (s -> a) -> n a
+    gets' f = nat $ gets f
+    put' :: s -> n Unit
+    put' s = nat $ put s
+    modify' :: (s -> s) -> n Unit
+    modify' f = nat $ modify f
+
+-- | Change the state type of a `MonadStateV`, given a morphism a lens.
+mapState :: forall s t m.
+  Monad m =>
+  Lens' s t -> MonadStateV s m -> MonadStateV t m
+mapState l { state, get, gets, put, modify } =
+  { state: state'
+  , get: get'
+  , gets: gets'
+  , put: put'
+  , modify: modify'
+  }
+  where
+    state' :: forall a. (t -> Tuple a t) -> m a
+    state' f = state g
+      where
+        g :: s -> Tuple a s
+        g = l f
+    get' :: m t
+    get' = gets (view l :: s -> t)
+    gets' :: forall a. (t -> a) -> m a
+    gets' f = gets ((view l >>> f) :: s -> a)
+    put' :: t -> m Unit
+    put' t = modify $ set l t
+    modify' :: (t -> t) -> m Unit
+    modify' f = modify $ over l f
---
+++ src/FocusUI.purs
@@ -0,0 +1,81 @@
+module FocusUI (StateRenderer, patchRepeatedly) where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Ref (REF, modifyRef, newRef, readRef)
+import Control.Monad.State.Reified (MonadStateV, fromGetPutFunctions, mapState)
+import DOM (DOM)
+import DOM.Event.Event (Event)
+import DOM.Node.Types (Node)
+import Optic.Core (view)
+import Optic.Types (Lens')
+import Text.Smolder.Markup (Markup)
+import Text.Smolder.Renderer.IncrementalDom (render)
+import Web.IncrementalDOM (patch)
+
+-- | `onStateChange listener action statev`: Run the stateful effect `action`
+-- | with `statev` and run the action given by `listener` whenever the state
+-- | changes. Whenever `action` calls `put`, `modify` or `state`, `listener` is
+-- | called with the new value. Using `put`, `modify`, `state` whitin the
+-- | listener directly will result into an infinite loop, so `listener` should
+-- | only use `get` or `gets`. Only after the `listener` has run is the new
+-- | value set as new state.
+onStateChange ::
+  forall m s a. Monad m =>
+  (s -> MonadStateV s m -> m Unit) ->
+  (MonadStateV s m -> m a) ->
+  MonadStateV s m -> m a
+onStateChange listener stateAction { get, put } =
+  stateAction $ monadStateVal unit
+  where
+    put' :: s -> m Unit
+    put' s = do
+      listener s $ monadStateVal unit
+      put s
+      -- TODO: also implement modify, state in terms of the given ones instead
+      -- of using defaults?
+
+    -- with trivial parameter because of circular dependency
+    monadStateVal :: Unit -> MonadStateV s m
+    monadStateVal unit = fromGetPutFunctions get put'
+
+-- | Create a new reified MonadState interface within the `Eff` monad with
+-- | given initial state.
+makeEffState ::
+  forall e s. s -> Eff (ref :: REF | e) (MonadStateV s (Eff (ref :: REF | e)))
+makeEffState initialState = do
+  stateRef <- newRef initialState
+  let
+    get = readRef stateRef
+    put s = modifyRef stateRef $ const s
+  pure $ fromGetPutFunctions get put
+
+-- | A renderer of some state `s` into `Markup`, with stateful actions running
+-- | in monad `m`.
+type StateRenderer s m = s -> MonadStateV s m -> Markup (Event -> m Unit)
+
+-- | Turn a renderer of a `t` into a renderer of an `s` via a `Lens'` from `s`
+-- | to `t`, ignoring all information contained in an `s` that is not present
+-- | in `t`.
+embed ::
+  forall s t m. Monad m =>
+  Lens' s t -> StateRenderer t m -> StateRenderer s m
+embed l r s monadStateV = r (view l s :: t) (mapState l monadStateV)
+
+-- | `patchRepeatedly node initialState renderer`: Render `initialState` via
+-- | `renderer` into `node` and update the interface whenever the state
+-- | changes.
+patchRepeatedly :: forall s e.
+  Node ->
+  s ->
+  StateRenderer s (Eff (dom :: DOM, ref :: REF | e)) ->
+  Eff (dom :: DOM, ref :: REF | e) Unit
+patchRepeatedly node initialState renderer = do
+  stateSig <- makeEffState initialState
+  onStateChange patchNode (patchNode initialState) stateSig
+  where
+    patchNode ::
+      s ->
+      MonadStateV s (Eff (dom :: DOM, ref :: REF | e)) ->
+      (Eff (dom :: DOM, ref :: REF | e)) Unit
+    patchNode s sig = patch node $ render $ renderer s sig
